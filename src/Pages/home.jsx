f4921c2e8743ea5ec375b229b36bfd6ac91ce853
Joshua Schappel <jmschappel12@gmail.com>
2019-12-04 12:48:24 -0500

    Added input field

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -6,19 +6,41 @@ class Home extends Component {
     constructor() {
         super()
         this.state = {
-            name: "Josh"
+            name: "",
+            inputField: ""
         }
+
+        this.handleClick = this.handleClick.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleClick(event) {
+        this.setState({name: this.state.inputField})
+    }
+
+    handleChange(event) {
+
+        console.log(event)
+        const name = event.target.name
+        const value = event.target.value
+        this.setState({
+            [name]: value
+        })
     }
 
     render() {
         return (
-            <body>
                 <div className="fullscreen">
                     <div className="row justify-content-center" style={{backgroundColor:"red"}}>
                         <h1>Hello {this.state.name}, Welcome to the home page</h1>
                     </div>
+                    <div className="row justify-content-center">
+                        <input type="text" value={this.state.inputField} onChange={this.handleChange}  name="inputField" placeholder="Type Here" className="form-control text-center" style={{width: "200px"}}/>
+                    </div>
+                    <div className="row justify-content-center">
+                        <button className="btn btn-primary"onClick={this.handleClick}>Press me!</button>
+                    </div>
                 </div>
-            </body>
         )
     }
 }
