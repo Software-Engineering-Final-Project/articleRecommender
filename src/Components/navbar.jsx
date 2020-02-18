abde1a712aa5257bdd278d42cf3da84e81ecb1a3
Joshua Schappel <jmschappel12@gmail.com>
2020-02-18 16:41:15 -0500

    Nav bar layout complete. Still ned to make sure the login state is passed around

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { useHistory } from 'react-router-dom'
 
 class Navbar extends Component {
 
     constructor(props) {
         super(props)
 
-        this.singInIcon = <FontAwesomeIcon icon={['fas', 'singInAlt']} />
+        this.singInIcon = <FontAwesomeIcon icon='sign-out-alt' />
 
     }
-
 
     render() {
         return (
@@ -20,31 +19,93 @@ class Navbar extends Component {
                         <h4 className="text-white">SearchIt</h4>
                         <span className="text-muted">A modern search browser for ASD related topics.</span>
                         <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Starred Articles</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Edit Profile</a>
-                        </li>
+                        <ListItem
+                            text="Home"
+                            path='/testSearch'
+                        />
+
+                        <ListItem
+                            text="Starred Articles"
+                            path='/favArticles'
+                        />
+
+                        <ListItem
+                            text="Edit Profile"
+                            path='/profile'
+                        />
                         </ul>
                     </div>
-                   
                 </div>
-                <nav className="navbar navbar-dark bg-dark">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <a className="navbar-brand" href="#">
+                <nav className="navbar navbar-dark" style={{'backgroundColor': '#60b0f4'}}>
+                    <NavPullDown />
+                    <a className="navbar-brand" href="/profile">
                         <img src={this.props.picture} width="30" height="30" className="d-inline-block rounded-circle align-top mr-2" alt="" />
-                        {this.props.user}
+                            {this.props.user}
                     </a>
+                    <LogoutButton />
                 </nav>
             </div>
         )
     }
+}
+
+
+/**
+ * Creates a list item, where the path is a path to the redirect. It requires the following props:
+ * 1) path: The path of the redirect
+ * 2) active: Boolean -> true if the link is the active link
+ * 3) text: The text to be displayed
+ */
+function ListItem(props) {
+    let history = useHistory()
+    const act = props.active === true ? 'active' : ''
+    return(
+        <li className={`nav-item active ${act}`}>
+            <a className="nav-link text-light" href='javascript:void(0);' onClick={() => history.push(props.path)}>{props.text}</a>
+        </li>
+    )
+}
+
+/**
+ * Redirects the user to the home page and logs them out.
+ */
+function LogoutButton() {
+    let history = useHistory()
+    const signOutIcon = <FontAwesomeIcon icon='sign-out-alt' />
+   
+    //TODO:Add set logout prop to true
+    return(
+    <button type="button" class="btn btn-outline-light" onClick={() => history.push('/')}>{signOutIcon}</button>
+    )
+
+}
+
+
+function NavPullDown() {
+    return (
+        <button 
+            className="navbar-toggler" 
+            type="button" 
+            data-toggle="collapse" 
+            data-target="#navbarToggleExternalContent" 
+            aria-controls="navbarToggleExternalContent" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+    )
+}
+
+/**
+ * Sends the user to the edit profile page
+ */
+function ProfileButton(component) {
+    let history = useHistory()
+    
+    //TODO: Push the login status to the account page
+    return (
+        <button type="button" onClick={() => history.push('/account')}  className={'btn btn-outline-light'}>{component}</button>
+    )
 }
 
 export default Navbar
--- src/fontAwesome.js
+++ src/fontAwesome.js
@@ -3,10 +3,10 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 
 // import your icons
 //import {} from '@fortawesome/free-regular-svg-icons'; // regular
-import { faSignInAlt } from '@fortawesome/free-solid-svg-icons' // solid
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons' // solid
 import {faGithub} from '@fortawesome/free-brands-svg-icons' // brand
 
 library.add(
     faGithub,
-    faSignInAlt
+    faSignOutAlt
 );
