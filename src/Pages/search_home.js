97184c83a7831bcda111b9e715077bee18a3a066
sachinmahashabde <mahashsa@shu.edu>
2020-02-19 10:17:54 -0500

    Added search page
    try it out, its pretty basic lookin

--- src/Pages/search_home.js
+++ src/Pages/search_home.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Navbar from '../Components/navbar'
 import BabyYoda from '../Images/BabyYoda.jpg'
+import Login from '../Components/form_login'
+import logo from '../Images/Logo.PNG'
+import CreateAccountRedirect from '../Components/account_create_link'
+import Footer from '../Components/footer_contact'
 
 class HomeSearch extends Component {
 
@@ -10,10 +14,29 @@ class HomeSearch extends Component {
 
     render() {
         return(
-            <Navbar
-                user="Joshua Schappel"
-                picture={BabyYoda}
-            />
+        <Fragment>
+        <Navbar 
+            user="Baby Yoda"
+            picture={BabyYoda}/>
+    <div className='container-fluid vertical-center'>
+            <div className='container-fluid'>
+                <div className='col justify-content-center mb-3 text-center col-form-label-lg'>
+        
+         
+                <form>
+  <div class="form-group">
+    <label for="col-form-label-lg text-center" > Search Here</label>
+    <input type="text" class="form-control" id="SearchBar" aria-describedby="searchHelp" placeholder="Search Here"></input>
+    <small id="searchHelp" class="form-text text-muted">Your searches are based on your preferences. See profile for more information.</small>
+     </div>
+	
+	
+    </form>
+    </div>
+	
+    </div>
+    </div>
+    </Fragment>  
         )
     }
 
